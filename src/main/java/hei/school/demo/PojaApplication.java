bb3aa9f44270305680a05be507b610903c2c80d3
Rindraniaina Rasolofondrasoa <rindraniainarasolofondrasoa17@gmail.com>
2026-06-08 11:28:56 +0300

    feat: app.properties config

--- src/main/java/hei/school/demo/PojaApplication.java
+++ src/main/java/hei/school/demo/PojaApplication.java
@@ -3,11 +3,17 @@ package hei.school.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @PojaGenerated
 public class PojaApplication {
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
     SpringApplication.run(PojaApplication.class, args);
   }
 }
