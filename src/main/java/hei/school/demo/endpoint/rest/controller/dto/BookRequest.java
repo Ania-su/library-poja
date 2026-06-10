2f5f1a8014251ec0ba93c82fea3de3d1968d79f5
Tsiry <tsiryras3@gmail.com>
2026-06-10 21:15:38 +0300

    feat: add BookRequest in dto

---
+++ src/main/java/hei/school/demo/endpoint/rest/controller/dto/BookRequest.java
@@ -0,0 +1,15 @@
+package hei.school.demo.endpoint.rest.controller.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class BookRequest {
+  private String title;
+  private String description;
+  private LocalDate publicationDate;
+  private List<String> authorIds;
+  private List<String> genreIds;
+}
