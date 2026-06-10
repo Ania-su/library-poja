be639c1268b7302c52d7e1791184ec4a5d32b91a
Ania-su <antsaaa.randria@gmail.com>
2026-06-10 21:20:19 +0300

    feat: delete a book service + controller

--- src/main/java/hei/school/demo/endpoint/rest/controller/BookController.java
+++ src/main/java/hei/school/demo/endpoint/rest/controller/BookController.java
@@ -8,13 +8,7 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -72,6 +66,16 @@ public class BookController {
     try {
       Book updated = bookService.updateBook(id, book);
       return ResponseEntity.ok(updated);
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+  }
+
+  @DeleteMapping("/book/{id}")
+  public ResponseEntity<?> deleteBook(@PathVariable String id) {
+    try {
+      bookService.deleteBook(id);
+      return ResponseEntity.noContent().build();
     } catch (Exception e) {
       return ResponseEntity.internalServerError().body(e.getMessage());
     }
