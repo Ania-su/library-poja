8366a8247a1c9e0eaaf8c55925a8321e3d703d72
Ania-su <antsaaa.randria@gmail.com>
2026-06-18 22:47:35 +0300

    chore: bad request exception

--- src/main/java/hei/school/demo/endpoint/rest/controller/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/hei/school/demo/endpoint/rest/controller/exceptionHandler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package hei.school.demo.endpoint.rest.controller.exceptionHandler;
 
+import hei.school.demo.exception.BadRequestException;
 import hei.school.demo.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.prefs.BackingStoreException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +15,11 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(NotFoundException.class)
   public ResponseEntity<String> handleNotFound(NotFoundException e) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<String> handleBadRequestException(BadRequestException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
   }
 
   @ExceptionHandler(Exception.class)
