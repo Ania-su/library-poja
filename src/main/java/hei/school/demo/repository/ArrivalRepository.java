35616befcb8dfbb4a43693ac0668d2db9d10f1a1
Rindraniaina Rasolofondrasoa <rindraniainarasolofondrasoa17@gmail.com>
2026-06-22 11:53:43 +0300

    style: format arrival repository

--- src/main/java/hei/school/demo/repository/ArrivalRepository.java
+++ src/main/java/hei/school/demo/repository/ArrivalRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArrivalRepository extends JpaRepository<JArrival, UUID> {
-}
+public interface ArrivalRepository extends JpaRepository<JArrival, UUID> {}
