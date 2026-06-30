28dd1abc6c6a7ecaaf5c17a63605ae57f649532a
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-06-30 12:40:11 +0300

    feat: jwt is set as cookie instead of raw text response

--- src/main/java/hei/school/demo/endpoint/rest/controller/AuthController.java
+++ src/main/java/hei/school/demo/endpoint/rest/controller/AuthController.java
@@ -5,6 +5,11 @@ import hei.school.demo.service.AuthService;
 import hei.school.demo.service.EmailAlreadyTakenException;
 import hei.school.demo.service.InvalidCredentialsException;
 import lombok.AllArgsConstructor;
+
+import java.time.Duration;
+import org.springframework.http.HttpHeaders;
+
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +24,22 @@ public class AuthController {
   @PostMapping("/register")
   public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
     try {
+
       String token = authService.register(request.getEmail(), request.getPassword());
-      return ResponseEntity.ok(token);
+      
+      ResponseCookie cookie = ResponseCookie.from("jwt", token)
+      .httpOnly(true)
+      .secure(true)
+      .path("/")
+      .maxAge(Duration.ofHours(1))
+      .sameSite("Strict")
+      .build();
+      
+      return ResponseEntity.status(201)
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
+            .header("Content-type", "text/plain")
+            .body("Register successfully");
+      
     } catch (EmailAlreadyTakenException e) {
       return ResponseEntity.status(409).body("Email already taken");
     } catch (InvalidCredentialsException e) {
@@ -32,7 +51,20 @@ public class AuthController {
   public ResponseEntity<String> login(@RequestBody RegisterRequest request) {
     try {
       String token = authService.login(request.getEmail(), request.getPassword());
-      return ResponseEntity.ok(token);
+
+      ResponseCookie cookie = ResponseCookie.from("jwt", token)
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(Duration.ofHours(1))
+            .sameSite("Strict")
+            .build();
+            
+      return ResponseEntity.status(200)
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
+            .header("Content-type", "text/plain")
+            .body("Login successfully");
+
     } catch (InvalidCredentialsException e) {
       return ResponseEntity.status(401).body("Invalid credentials");
     }
